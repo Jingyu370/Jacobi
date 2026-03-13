bbeecbc4b8fc0cf4266058bbe9e5c385d07b9af3
Jingyu370 <2024210378@stu.cqupt.edu.cn>
2026-03-13 14:43:52 +0800

    first version

---
+++ Jacobi.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <iomanip>
+#include <string>
+#include "mpi.h"
+
+using namespace std;
+
+const int totalsize = 16;    // 全局矩阵列数（可变）
+const int step = 10;         // 迭代步数
+
+const int tag1 = 3;
+const int tag2 = 4;
+
+int main(int argc, char **argv){
+    int rank, size;
+    int upper, lower;
+    int begin_row = 1, end_row;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    const int base_size = totalsize / size;
+    const int remainder = totalsize % size;
+    
+    //修改：当线程数无法整除时，按顺序均分
+    int mysize;
+    if (rank < remainder) {
+        mysize = base_size + 1;
+    } else {
+        mysize = base_size;
+    }
+
+    MPI_Status status;
+    end_row = mysize;
+
+    vector<vector<double>> a(mysize + 2, vector<double>(totalsize, 0.0));
+    vector<vector<double>> b(mysize + 2, vector<double>(totalsize, 0.0));
+
+    for(int i = 0; i < totalsize; ++i){
+        a[0][i] = 8.0;
+        a[mysize + 1][i] = 8.0;
+    }
+ 
+    for (int i = 0; i < mysize + 2; ++i) {
+        a[i][0] = 8.0;
+        a[i][totalsize - 1] = 8.0;
+    }
+
+    if(rank == 0){
+        begin_row = 2;
+        for(int j = 0; j < totalsize ; ++j){
+            a[1][j] = 8.0; 
+        }
+    }
+
+    if(rank == size - 1){
+        end_row = mysize - 1;
+        for(int j = 0; j < totalsize ; ++j){
+            a[mysize][j] = 8.0;
+        }
+    }
+
+    upper = (rank > 0) ? rank - 1 : MPI_PROC_NULL;
+    lower = (rank < size - 1) ? rank + 1 : MPI_PROC_NULL;
+
+    for(int n = 0; n < step; ++n){
+        MPI_Sendrecv(&a[1][0], totalsize, MPI_DOUBLE, upper, tag1,
+                    &a[mysize + 1][0], totalsize, MPI_DOUBLE, lower, tag1,
+                    MPI_COMM_WORLD, &status);
+
+        MPI_Sendrecv(&a[mysize][0], totalsize, MPI_DOUBLE, lower, tag2,
+                    &a[0][0], totalsize, MPI_DOUBLE, upper, tag2,
+                    MPI_COMM_WORLD, &status);
+
+        for(int i = begin_row; i <= end_row; ++i){
+            for (int j = 1; j < totalsize - 1; ++j) {
+                b[i][j] = (a[i + 1][j] + a[i - 1][j] + a[i][j + 1] + a[i][j - 1]) * 0.25;
+            }
+        }
+
+        for (int i = begin_row; i <= end_row; ++i) {
+            for (int j = 1; j < totalsize - 1; ++j) {
+                a[i][j] = b[i][j];
+            }
+        }
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    string filename = "result/result_" + to_string(size) + "proc_" + 
+                to_string(totalsize) + "rows_" + to_string(step) + "steps.txt";
+    if (rank == 0) {
+        ofstream outfile(filename);
+        if (outfile.is_open()) {
+            outfile << "==================================== Jacobi迭代结果汇总 ====================================" << endl;
+            outfile << setw(10) << "进程ID" << setw(10) << "本地行号" << setw(8) << "列";
+            for (int j = 0; j < totalsize; ++j) {
+                outfile << setw(8) << j;
+            }
+            outfile << endl;
+            outfile << "--------------------------------------------------------------------------------------------" << endl;
+            outfile.close();
+        }
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    for (int myid = 0; myid < size; ++myid) {
+        if (rank == myid) {
+            ofstream outfile(filename, ios::app);
+            if (outfile.is_open()) {
+                for (int i = 1; i <= mysize; ++i) {
+                    outfile << setw(10) << myid << setw(10) << i << setw(8) << ":";
+                    for (int j = 0; j < totalsize; ++j) {
+                        outfile << setw(8) << fixed << setprecision(2) << a[i][j];
+                    }
+                    outfile << endl;
+                }
+                outfile.close();
+            }
+        }
+        MPI_Barrier(MPI_COMM_WORLD);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
